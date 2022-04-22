68629057c40295c95ca13664b7f67242f98adf20
leflores-fisi <paolo.flores2@unmsm.edu.pe>
2022-04-21 21:02:30 -0500

    Bind form values and add form validation for post accidents

--- addAccident.php
+++ addAccident.php
@@ -1,21 +1,40 @@
-<?php if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+<?php
   require_once "database.php";
+  $submit_error = null;
 
-  $new_accident = $_POST;
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $new_accident = $_POST;
 
-  # Extracting information
-  $title = $new_accident["title"];
-  $date  = $new_accident["date"];
-  $level = intval($new_accident["level"]);
-  $description = $new_accident["description"];
+    # Extracting information
+    $title = $new_accident["title"];
+    $date  = $new_accident["date"];
+    $level = intval($new_accident["level"]);
+    if ($level < 1 || $level > 3) $level = 1;
+    $description = $new_accident["description"];
 
-  # Inserting a new row TODO: validations
-  $insert_statement = $DBconnection->prepare("INSERT INTO accidents (title, date, level, description) VALUES ('$title', '$date', $level, '$description')");
-  $insert_statement->execute();
+    $submit_error = null;
 
-  header("Location: index.php"); # redirection
-} ?>
+    if (empty($title) || empty($date) || empty($level) || empty($description)) {
+      $submit_error = "All inputs must be filled";
+    }
+    else if (!strtotime($date)) {
+      $submit_error = "Date must have a valid format";
+    }
+    else {
+      # Inserting a new row TODO: validations
+      $insert_statement = $DBconnection->prepare("INSERT INTO accidents
+        (title, date, level, description) VALUES (:title, :date, :level, :description)");
+
+      $insert_statement->bindParam("title", $title);
+      $insert_statement->bindParam("date", $date);
+      $insert_statement->bindParam("level", $level);
+      $insert_statement->bindParam("description", $description);
+      $insert_statement->execute();
+
+      header("Location: index.php"); # redirection
+    }
+  }
+?>
 
 <?php require_once "partials/header.php" ?>
 <main class="container m-auto px-16">
@@ -61,10 +80,16 @@
         </div>
       </div>
     </div>
+
     <div class="w-full mb-6">
       <label for="description" class="block mb-2 text-sm font-medium text-gray-800">Describe the accident</label>
       <textarea name="description" class="block w-full h-40 px-4 py-2 text-gray-700 bg-white border rounded-md focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"></textarea>
     </div>
+
+    <?php if ($submit_error): ?>
+    <div class="text-red-600"><?= $submit_error ?></div>
+    <?php endif ?>
+
     <button type="submit" class="m-auto w-32 justify-center border-y border-x border-black cursor-pointer flex items-center px-2 py-2 font-normal tracking-wide capitalize transition-colors duration-200 transform bg-white hover:bg-gray-100 rounded-md focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
       <svg class="w-5 h-5 mx-1 text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
