3600c701000ae8f482db77de691fec901a5e6ea2
leflores-fisi <paolo.flores2@unmsm.edu.pe>
2022-04-25 00:59:06 -0500

    Move register and login buttons to header

--- index.php
+++ index.php
@@ -8,12 +8,6 @@
     <h1 class="text-4xl mt-16 mb-4">Days without...</h1>
     <span>Start counting and saving your accidents</span>
     <div class="block justify-center">
-      <a class="inline-block m-auto bg-gray-200 font-thin w-fit border border-gray-600 px-6 py-1 mt-4 text-gray-600 rounded-sm" href="register.php">
-        Create an account
-      </a>
-      <a class="inline-block m-auto bg-gray-100 font-thin w-fit border border-gray-600 px-6 py-1 mt-4 text-gray-600 rounded-sm" href="login.php">
-        Login
-      </a>
     </div>
   </div>
 </main>
--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,6 @@
+<?php
+  $fileName = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1);  
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,26 +14,34 @@
 
 <body class="h-full min-h-full">
   <header>
-
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-800">
       <div class="container flex flex-wrap justify-between items-center mx-auto">
         <a href="index.php" class="flex items-center">
           <img src="public/assets/logo.png" class="mr-3 h-6 sm:h-9 w-8" alt="DWA Logo" />
           <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Days without</span>
         </a>
-        <div class="flex md:order-2">
-          <div class="hidden relative mr-3 md:mr-0 md:block">
-            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
-              </svg>
+        <?php if ($fileName != "login.php" && $fileName != "register.php"): ?>
+          <div class="flex md:order-2">
+            <div class="hidden relative mr-3 md:mr-0 md:block">
+              <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+              <input type="text" id="email-address-icon" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
             </div>
-            <input type="text" id="email-address-icon" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
+            <?php if (isset($_SESSION["user"])): ?>
+              <a href="logout.php">Logout</a>
+            <?php else: ?>
+              <a class="inline-block m-auto bg-gray-100 font-thin w-fit border border-gray-600 px-6 mx-2 py-1 text-sm text-gray-600 rounded-sm" href="login.php">
+                Log in
+              </a>
+              <a class="inline-block m-auto bg-gray-200 font-thin w-fit border border-gray-600 px-6 py-1 text-sm text-gray-600 rounded-sm" href="register.php">
+                Try for free
+              </a>
+            <?php endif ?>
           </div>
-          <?php if (isset($_SESSION["user"])): ?>
-            <a href="logout.php">Logout</a>
-          <?php endif ?>
-        </div>
+        <?php endif ?>
       </div>
     </nav>
   </header>
