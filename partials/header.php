28fab160367fad84bce127294fff2fcf036427ba
leflores-fisi <paolo.flores2@unmsm.edu.pe>
2022-04-28 05:13:11 -0500

    Fetch tailwind without redirection

--- partials/header.php
+++ partials/header.php
@@ -9,7 +9,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Days without accidents</title>
-  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="https://cdn.tailwindcss.com/3.0.24"></script>
   <link rel="stylesheet" href="static/css/main.css">
 </head>
 
